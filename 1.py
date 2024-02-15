2821a256b9a1c0efe59b0a0c4774da2d09dbc316
Fallods <ericc404@nycstudents.net>
2024-02-15 12:51:38 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,32 +1,24 @@
-#https://www.hackerrank.com/contests/projecteuler/challenges/euler001/problem?isFullScreen=true
 
-#If we list all the natural numbers below  that are multiples of 3 or 5 , we get 3,5,6,9. The sum of these multiples is 23 .
-
-#Find the sum of all the multiples of 3 or 5 below N.
-
-multiple = []
 tests = []
 testcase = int(input('How much test cases: '))
 
 for i in range(1,testcase+1):
-    x = input(f'What is your {i} test case?\n > ')
+    x = int(input(f'What is your {i} test case?\n > '))
     tests.append(x)
 
-N = int(input("Your Number: "))
-sum = 0
-def multiplesum():
+def n(N):
+    multiple = []
+    sum = 0
     for i in range(1,N):
         if i%3 == 0:
             multiple.append(int(i))
         elif i%5 == 0:
             multiple.append(int(i))
-
     for i in range(0,len(multiple)):
         sum += multiple[i]
 
-print(tests)
+    print(sum)
     
 
-
-
-
+for i in tests:
+    n(i)
