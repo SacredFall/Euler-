c04b7357f084292719c0502e19038670d1642781
Fallods <ericc404@nycstudents.net>
2024-02-18 23:09:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-num = 600851475143
-# If given number is greater than 1
-if num > 1:
-    # Iterate from 2 to n / 2
-    for i in range(2, int(num/2)+1):
-        # If num is divisible by any number between
-        # 2 and n / 2, it is not prime
-        if (num % i) == 0:
-            print(num, "is not a prime number")
-            break
-    else:
-        print(num, "is a prime number")
-else:
-    print(num, "is not a prime number")
+
+#x mod 3 = 0                    x mod7 =2                      x mod 8 =1                 x mod 11 = 3
+for x in range (10000):
+    if x%3 == 0 and x%7 == 2 and x%8 == 1 and x%11 == 3:
+        print (x)
