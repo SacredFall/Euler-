258fd9e1e0a421f5de48b3277a60c7b71b69e9c3
Eric Chen <ericc404@nycstudents.net>
2024-03-11 13:58:21 -0400

    na

---
+++ MrWhalenHas0DollarsAndHeStealsMoneyFromC.py
@@ -0,0 +1,7 @@
+MrWhalenHas0DollarsAndHeStealsMoneyFromChildrenToMakeLiving = [['robux','1','8','3'],['2','5','7']]
+
+
+y = [[int(i) for i in x] for x in MrWhalenHas0DollarsAndHeStealsMoneyFromChildrenToMakeLiving]
+
+print(y)
+    
---
+++ smallestmultiple.py
@@ -0,0 +1,4 @@
+for j in range(10000):
+    for i in range(2,21):
+        if j%i == 0:
+            print(j)
