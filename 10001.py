217c538724b4b417d149cf657e090f6a60ebcbe4
Eric Chen <ericc404@nycstudents.net>
2024-03-12 13:44:28 -0400

    done

--- 10001.py
+++ 10001.py
@@ -1,10 +1,21 @@
-#10001st prime
+#10001st prim
 
-def primechecking():  
-    for i in factors:
-        c=0
-        for j in range(1,i):
-            if i%j==0:
-                c += 1
-        if c==1:
-            primefactors.append(i)
+
+def prime(n):
+    if n > 1:
+
+        for i in range(2, int(n/2)+1):
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (n % i) == 0:
+                return False
+        else:
+            return True
+seen = 0
+n = 1
+while seen < 10001:
+    n += 1
+    if prime(n):
+        seen += 1
+
+print(n)
--- PrimeNumbers1.py
+++ PrimeNumbers1.py
@@ -1,4 +1,4 @@
-num = 600851475143
+num = 4
 # If given number is greater than 1
 if num > 1:
     # Iterate from 2 to n / 2
@@ -6,9 +6,8 @@ if num > 1:
         # If num is divisible by any number between
         # 2 and n / 2, it is not prime
         if (num % i) == 0:
-            print(num, "is not a prime number")
-            break
+            return False
     else:
-        print(num, "is a prime number")
+        return True
 else:
     print(num, "is not a prime number")
